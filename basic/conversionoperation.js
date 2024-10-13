fec8e6942b390ed4774f784b7b2bb2fc0d8f3267
Jimit Karangia <jimitk21@gmail.com>
2024-10-13 10:10:39 +0530

    operation added

--- basic/conversionoperation.js
+++ basic/conversionoperation.js
@@ -25,3 +25,48 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+/*    Operations    */
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%2) */
+
+let str1 = "hello"
+let str2 = "jimit"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true)
+//console.log(true+) // This will throw error
+console.log(+"") // op will be 0
+
+let num1,num2, num3 
+
+num1 = num2 = num3 = 2 + 3 //(This kind of coding is not preferred)
+
+// prefix postfix
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter) // op will be 101
+
+++gameCounter;
+console.log(gameCounter) 
